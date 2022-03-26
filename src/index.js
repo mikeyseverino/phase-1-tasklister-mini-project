ac998e885393c2a08f6461e0723f0b97dd04a6be
Michael Severino <mikeyseverino@gmail.com>
2022-03-26 17:30:58 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newTaskDescription = document.getElementById("new-task-description")
+    buildToDo(newTaskDescription.value)
+})
+
+})
+function buildToDo(toDo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  p.textContent = `${toDo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
